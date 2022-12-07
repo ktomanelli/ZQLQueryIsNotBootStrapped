a4247202f80b20f9a255d4a96fd74e03a06929a8
Kyle Tomanelli <15216567+ktomanelli@users.noreply.github.com>
2022-12-07 09:56:13 -0500

    base usage with error

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using TestServer.Client;
+try
+{
+    var httpClient = new HttpClient();
+    httpClient.BaseAddress = new Uri("http://localhost:10000/graphql");
+
+    var client = new TestServerGraphQLClient(httpClient);
+
+    var response = await client.Query(static o => o.Me(o => new { o.Id, o.FirstName, o.LastName }));
+
+    Console.WriteLine($"GraphQL: {response.Query}"); // GraphQL: query { me { id firstName lastName } }
+    Console.WriteLine($"{response.Data.Id}: {response.Data.FirstName} {response.Data.LastName}"); // 1: Jon Smith
+}catch(Exception e)
+{
+    Console.WriteLine(e);
+}
